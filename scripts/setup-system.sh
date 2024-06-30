973996841a28f5081b2420ac874680e042a66575
Luka Panio <lukapanio@gmail.com>
2024-06-30 12:55:57 +0200

    Add extra tools and fix dns

--- scripts/setup-system.sh
+++ scripts/setup-system.sh
@@ -7,3 +7,7 @@ echo "127.0.0.1 $1" >> /etc/hosts
 sed -i -e '/en_US\.UTF-8/s/^# //g' /etc/locale.gen
 locale-gen
 update-locale LANG=en_US.UTF8
+
+# Fix resolv conf
+rm /etc/resolv.conf
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
