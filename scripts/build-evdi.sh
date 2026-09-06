ccb9be0f7043d71bfd2cf05e71e9a56462037435
Fauzan Amir Al Ghiffary <56854433+alghiffaryfa19@users.noreply.github.com>
2026-09-06 18:47:20 +0700

    Update build script for evdi_bridge
    Install necessary packages and adjust paths for building evdi_bridge.

--- scripts/build-evdi.sh
+++ scripts/build-evdi.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 echo "Building evdi_bridge..."
-cd /usr/src/evdi_bridge
+apt install -y git build-essential libdrm-dev
+cd overlay/usr/src/evdi_bridge
 gcc evdi_bridge.c -o /usr/bin/evdi_bridge -ldrm
 chmod +x /usr/bin/evdi_bridge
 echo "evdi_bridge built successfully!"
+cp overlay/usr/lib/systemd/system/evdi-bridge.service /usr/lib/systemd/system/
 systemctl enable evdi-bridge
