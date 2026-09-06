20f19914d2ec5adce0ef39763d681d448ab5ace4
Ozan <alghiffaryfa19@gmail.com>
2026-09-06 19:42:39 +0700

    Fix build: add -levdi to linker flags

--- scripts/build-evdi.sh
+++ scripts/build-evdi.sh
@@ -8,14 +8,14 @@ apt install -y git build-essential libdrm-dev libevdi-dev
 if [ -d "overlay/usr/src/evdi_bridge" ]; then
     echo "Detected manual build from cloned repository..."
     cd overlay/usr/src/evdi_bridge
-    gcc evdi_bridge.c -o /usr/bin/evdi_bridge -ldrm
+    gcc evdi_bridge.c -o /usr/bin/evdi_bridge -ldrm -levdi
     chmod +x /usr/bin/evdi_bridge
     cd ../../../..
     cp overlay/usr/lib/systemd/system/evdi-bridge.service /usr/lib/systemd/system/
 elif [ -d "/usr/src/evdi_bridge" ]; then
     echo "Detected debos rootfs build..."
     cd /usr/src/evdi_bridge
-    gcc evdi_bridge.c -o /usr/bin/evdi_bridge -ldrm
+    gcc evdi_bridge.c -o /usr/bin/evdi_bridge -ldrm -levdi
     chmod +x /usr/bin/evdi_bridge
 else
     echo "Error: Cannot find evdi_bridge source code!"
